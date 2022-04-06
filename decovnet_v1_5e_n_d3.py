e15c87b5aa28ad09c6bd583877a89a64c7f7f6ab
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-06 13:47:33 +0100

    add more print

--- decovnet_v1_5e_n_d3.py
+++ decovnet_v1_5e_n_d3.py
@@ -267,13 +267,15 @@ class Up(nn.Module):
         # input is CHW
         diffY = torch.tensor([x2.size()[2] - x1.size()[2]])
         diffX = torch.tensor([x2.size()[3] - x1.size()[3]])
-
+        print('here 1')
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
                         diffY // 2, diffY - diffY // 2])
+        print('here 2')
         # if you have padding issues, see
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
         x = torch.cat([x2, x1], dim=1)
+        print('here 3')
         return self.conv(x)
 
 
