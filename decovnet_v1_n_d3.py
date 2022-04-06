ad051022b4167d8f7aeb3ae7746c02f2bb709645
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-06 19:07:39 +0100

    3 class version training for 300 epochs

--- decovnet_v1_n_d3.py
+++ decovnet_v1_n_d3.py
@@ -2140,7 +2140,7 @@ LR_DECAY = 1
 INIT_MODEL_PATH = 'ncov-Epoch_00140-auc95p9.pth'
 INIT_MODEL_STRICT = "True"
 SNAPSHOT_FREQ = 5
-TRAIN_EPOCH = 5 # 300 , will likely stop early
+TRAIN_EPOCH = 300 #, will likely stop it early
 SNAPSHOT_HOME = "experiments"
 SNAPSHOT_MODEL_TPL = "ncov-Epoch_{:05d}.pth"
 
