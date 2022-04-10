54a3fe71a3e7aca565dec31555d7c45136d24d1e
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-10 15:06:40 +0100

    test lung segmentation algo

---
+++ test_lungmask.py
@@ -0,0 +1,26 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from skimage import io
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import shutil
+import random
+import neptune.new as neptune
+
+random.seed(0); torch.manual_seed(0); np.random.seed(0)
+# device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+run = neptune.init(
+    project="lucypallent/research-project",
+    api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIzMmI3Y2EyOC1kZGMzLTRiNjgtYjY1MS04ZmZlMzA5MjJiYTYifQ==",
+)  # your credentials
+
+img = io.imread('/content/drive/MyDrive/curated_data/curated_data/1NonCOVID/N274_0.png')
+run.log_image('test_img', img)
+
+from lungmask import mask
+
+seg = mask.apply(img)
+run.log_image('tes_seg', seg)
