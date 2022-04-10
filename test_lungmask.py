c65302b29e1339b3e9c0ef36eef01bec090b929b
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-10 15:28:08 +0100

    typo

--- test_lungmask.py
+++ test_lungmask.py
@@ -7,20 +7,21 @@ import pandas as pd
 import numpy as np
 import shutil
 import random
-import neptune.new as neptune
+# import neptune.new as neptune
 
 random.seed(0); torch.manual_seed(0); np.random.seed(0)
 # device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-run = neptune.init(
-    project="lucypallent/research-project",
-    api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIzMmI3Y2EyOC1kZGMzLTRiNjgtYjY1MS04ZmZlMzA5MjJiYTYifQ==",
-)  # your credentials
+#
+# run = neptune.init(
+#     project="lucypallent/research-project",
+#     api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIzMmI3Y2EyOC1kZGMzLTRiNjgtYjY1MS04ZmZlMzA5MjJiYTYifQ==",
+# )  # your credentials
 
 img = io.imread('dataset3/1NonCOVID/N314_16.png')
-run.log_image('test_img', img)
+# run.log_image('test_img', img)
 
 from lungmask import mask
 
 seg = mask.apply(img)
-run.log_image('tes_seg', seg)
+# run.log_image('tes_seg', seg)
+print('works')
