5a5255edb80eb18a97a830f3b0d07351adf64371
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-13 00:29:24 +0100

    dbg

--- dcvnt_v4_make_d5.py
+++ dcvnt_v4_make_d5.py
@@ -36,9 +36,9 @@ import numpy as np
 pth = 'COVID19_0001/COVID19-0101.dcm' # path is correct
 pth2 = '62448201'
 input_image = sitk.ReadImage(pth)
-print(input_image.pixel_array)
+print(type(input_image))
 input_image = sitk.ReadImage(pth2)
-print(input_image.pixel_array)
+print(type(input_image))
 model = mask.get_model('unet','R231CovidWeb')
 result = mask.apply(input_image, model)
 
