e115692bb5ea83bf85bff5dc5ce85cb655e9d09b
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-13 15:26:48 +0100

    typo

--- dcvnt_v4_make_d5.py
+++ dcvnt_v4_make_d5.py
@@ -59,7 +59,7 @@ ds.save_as('trial.dcm')
 # arr = arr.reshape(1, 512, 512)
 input_image = sitk.ReadImage('trial.dcm')
 model = mask.get_model('unet','R231CovidWeb')
-result = mask.apply(arr, model)#, noHU=True)
+result = mask.apply(input_image, model)#, noHU=True)
 print('model runs')
 
 print(result.max())
