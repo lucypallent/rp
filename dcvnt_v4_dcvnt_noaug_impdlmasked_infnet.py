4c0b26ab7c09a509d354c9ae9c7cf3a8a88e44eb
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-16 19:14:10 +0100

    displays the first slice

--- dcvnt_v4_dcvnt_noaug_impdlmasked_infnet.py
+++ dcvnt_v4_dcvnt_noaug_impdlmasked_infnet.py
@@ -1837,7 +1837,7 @@ for e in range(TRAIN_EPOCH):
         # tik = time.time()
 
         # display image
-        run['training/batch/img'].log(File.as_image(all_F[0,0]))
+        run['training/batch/img'].log(File.as_image(all_F[0,0,0]))
 
         preds = model([all_F.cuda(non_blocking=True)])   # I3D
         labels = all_L.cuda(non_blocking=True)
