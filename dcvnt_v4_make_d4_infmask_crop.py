c74af9c20a4a6aee4604fc29f8a9ab508934a11b
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-17 00:42:37 +0100

    print statement added

--- dcvnt_v4_make_d4_infmask_crop.py
+++ dcvnt_v4_make_d4_infmask_crop.py
@@ -97,6 +97,7 @@ def resize_cta_images(x):        # dtype is "PE"/"NORMAL"
     print(crop_box)
 
     print(raw_imgs.shape)
+    print(raw_imgs[:, :, 0])
     crop_imgs = raw_imgs[:, cropbox[0, 0]:cropbox[0, 1], cropbox[1, 0]:cropbox[1, 1]]
 
     print(raw_masks.shape)
