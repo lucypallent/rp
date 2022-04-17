476da7a986b752de3fb1f9a56b2b714cbd7759ac
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-17 15:27:27 +0100

    rerun to hopefully fix issue

--- dcvnt_v4_make_d4.py
+++ dcvnt_v4_make_d4.py
@@ -412,38 +412,19 @@ import cv2
 
 def resize_cta_images(x):        # dtype is "PE"/"NORMAL"
     print (x)
-    if os.path.isfile(os.path.join(des_home, x+".npy")) is True:
-        return
+    # if os.path.isfile(os.path.join(des_home, x+".npy")) is True:
+    #     return
     ### raw_imgs = np.uint8(np.load(os.path.join(src_home, x+".npy")))
     raw_imgs = np.load(os.path.join(src_home, x+"-2.npy")) # -2 is the img which appears like normal orig is blck sqr
     raw_masks = np.load(os.path.join(src_home, x+"-dlmask.npy"))
     length = len(raw_imgs)
 
-    # clip_imgs = raw_imgs[int(length*clip_range[0]):int(length*clip_range[1])]
-    ###
-    # clip_masks = raw_masks[int(length*clip_range[0]):int(length*clip_range[1])]
-    #
-    # raw_imgs = clip_imgs
-    # raw_masks = clip_masks
-    ###
-#####
-    # zz, yy, xx = np.where(raw_masks)
-    # cropbox = np.array([[np.min(zz), np.max(zz)], [np.min(yy), np.max(yy)], [np.min(xx), np.max(xx)]])
-    # crop_imgs = raw_imgs[cropbox[0, 0]:cropbox[0, 1],
-    #                      cropbox[1, 0]:cropbox[1, 1],
-    #                      cropbox[2, 0]:cropbox[2, 1]]
-    #
-    # crop_masks = raw_masks[cropbox[0, 0]:cropbox[0, 1],
-    #                       cropbox[1, 0]:cropbox[1, 1],
-    #                       cropbox[2, 0]:cropbox[2, 1]]
-    #
-    # raw_imgs = crop_imgs
-    # raw_masks = crop_masks
-#####
+
     height, width = raw_imgs.shape[1:3]
     zoomed_imgs = zoom(raw_imgs, (slice_resolution, new_height/height, new_width/width))
     np.save(os.path.join(des_home, x+".npy"), zoomed_imgs)
     zoomed_masks = zoom(raw_masks, (slice_resolution, new_height/height, new_width/width))
+    zoomed_masks = zoomed_masks.astype(np.float32)
     np.save(os.path.join(des_home, x+"-dlmask.npy"), zoomed_masks)
 
     immasks = np.concatenate([zoomed_imgs, zoomed_masks*255], axis=2)[length//2]
