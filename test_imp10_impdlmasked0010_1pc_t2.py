9a7318cb25a5d3340605f9f4c9c57263b49338dd
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-22 14:42:10 +0100

    dbg

--- test_imp10_impdlmasked0010_1pc_t2.py
+++ test_imp10_impdlmasked0010_1pc_t2.py
@@ -2020,6 +2020,7 @@ def test_model(model_pth, folder_pth, run):
     print(roc_auc_score(true, pred_probs, multi_class="ovo"))
 
     # plot the ROC Curve
+    n_classes = 3
     # Compute ROC curve and ROC area for each class
     fpr = dict()
     tpr = dict()
@@ -2049,6 +2050,62 @@ def test_model(model_pth, folder_pth, run):
     plt.title("Receiver operating characteristic example")
     plt.legend(loc="lower right")
     run["evaluation/ROC_curve"].log(File.as_image(fig))
+
+    # get the master graph
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Plot all ROC curves
+    plt.figure()
+    plt.plot(
+        fpr["micro"],
+        tpr["micro"],
+        label="micro-average ROC curve (area = {0:0.2f})".format(roc_auc["micro"]),
+        color="deeppink",
+        linestyle=":",
+        linewidth=4,
+    )
+
+    plt.plot(
+        fpr["macro"],
+        tpr["macro"],
+        label="macro-average ROC curve (area = {0:0.2f})".format(roc_auc["macro"]),
+        color="navy",
+        linestyle=":",
+        linewidth=4,
+    )
+
+    colors = cycle(["aqua", "darkorange", "cornflowerblue"])
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(
+            fpr[i],
+            tpr[i],
+            color=color,
+            lw=lw,
+            label="ROC curve of class {0} (area = {1:0.2f})".format(i, roc_auc[i]),
+        )
+
+    plt.plot([0, 1], [0, 1], "k--", lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("Some extension of Receiver operating characteristic to multiclass")
+    plt.legend(loc="lower right")
+    run["evaluation/ROC_curve_all"].log(File.as_image(fig))
+
 
     # get accuracy averaged out across class
     print(accuracy_score(true, pred))
