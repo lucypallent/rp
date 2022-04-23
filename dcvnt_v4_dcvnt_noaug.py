13df0535946de406ac76d054836ea6891ee98272
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-23 15:53:32 +0100

    Update dcvnt_v4_dcvnt_noaug.py
    dbg

--- dcvnt_v4_dcvnt_noaug.py
+++ dcvnt_v4_dcvnt_noaug.py
@@ -157,17 +157,17 @@ from scipy.ndimage import zoom
 #     sys.path.insert(0, "..")
 #     from ops.dataset_ops import Rand_Affine, Rand_Crop, Rand_Transforms
 
-# def Train_Collatefn(data):
-#     all_F, all_L, all_info = [], [], []
-#
-#     for i in range(len(data)):
-#         all_F.append(data[i][0])
-#         all_L.append(data[i][1])
-#         all_info.append(data[i][2])
-#     all_F = torch.cat(all_F, dim=0)
-#     all_L = torch.cat(all_L, dim=0)
-#     return all_F, all_L, all_info
-#
+def Train_Collatefn(data):
+    all_F, all_L, all_info = [], [], []
+
+    for i in range(len(data)):
+        all_F.append(data[i][0])
+        all_L.append(data[i][1])
+        all_info.append(data[i][2])
+    all_F = torch.cat(all_F, dim=0)
+    all_L = torch.cat(all_L, dim=0)
+    return all_F, all_L, all_info
+
 # # Rand_Affine
 # def Rand_Affine(img, ANGLE_R=10, TRANS_R=0.2, SCALE_R=0.3, SHEAR_R=15, FLIP_B=False):
 #     assert isinstance(img, Image.Image) or isinstance(img[0], Image.Image)
