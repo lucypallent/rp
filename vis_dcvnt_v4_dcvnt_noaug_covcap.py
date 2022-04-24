337499c8509f42f77e8401e34e1fe426357cea12
lucypallent <lucy.h.pallent@durham.ac.uk>
2022-04-24 13:21:13 +0100

    typo

--- vis_dcvnt_v4_dcvnt_noaug_covcap.py
+++ vis_dcvnt_v4_dcvnt_noaug_covcap.py
@@ -1938,7 +1938,7 @@ INIT_MODEL_PATH = 'ncov-Epoch_00140-auc95p9.pth'
 INIT_MODEL_STRICT = "True"
 SNAPSHOT_FREQ = 5
 TRAIN_EPOCH = 200 #, will likely stop it early
-SNAPSHOT_HOME = "experiments_v4_dcvnt_run2"
+SNAPSHOT_HOME = "experiments_v4_dcvnt_cvcp_run2"
 SNAPSHOT_MODEL_TPL = "ncov-Epoch_{:05d}.pth"
 
 
